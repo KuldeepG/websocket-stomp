72c920487410a3d120f7d08abd1c9af598eecf2d
Kuldeep ghogre <kuldeepghogre@Kuldeeps-MacBook-Pro.local>
2019-12-03 19:36:09 +0530

    Initial

---
+++ complete/src/main/java/hello/GreetingController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	@CrossOrigin("*")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
